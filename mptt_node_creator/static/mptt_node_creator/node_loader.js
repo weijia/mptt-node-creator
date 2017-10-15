69daf1fe9915f1bd99f55dfac7dccba24ddd695c
Richard <richardwangwang@gmail.com>
2017-10-15 19:56:24 +0800

    Use parent when doing AJAX.

--- mptt_node_creator/static/mptt_node_creator/node_loader.js
+++ mptt_node_creator/static/mptt_node_creator/node_loader.js
@@ -1,38 +1,42 @@
 $(function() {
-        var level = 1;
-
-        function attach_magic_suggest(div_elem){
+        function attach_magic_suggest(div_elem, parent){
             var elem = $("input", div_elem);
+            var additionalParam = {"parent": "None"};
+            if(parent){
+                additionalParam = {"parent": parent};
+            }
             var ms = elem.magicSuggest({
                 data: base_tree_url,
-//                allowDuplicates: false,
                 resultsField: "objects",
                 valueField: "id",
                 displayField: "content",
+                dataUrlParams: additionalParam,
                 method: "GET"
             });
-            div_elem.attr("ms", ms);
+//            div_elem.attr("ms", ms);
             $(ms).on("selectionchange", function(e, control, selects){
-
                     console.log(ms);
-
                     if(selects.length == 0){
                         return;
                     }
                     var next_level = parseInt(div_elem.attr("level"))+1;
                     var next_level_div_elem = $("#level-"+next_level);
                     if(next_level_div_elem.length>0){
-                        $("input", next_level_div_elem).focus();
-                        return
+                        setTimeout(function(){
+                            console.log("focus on", $("input", next_level_div_elem));
+                            $("input", next_level_div_elem).focus();
+                        }, 50);
+                        return;
                     };
 
+                    var parent = selects[0].id;
+
                     var new_div_with_input = $('<div class="col-md-2" level="'+
-                        next_level+'" id="level-'+next_level+'">'+
+                        next_level+'" id="level-'+next_level+'" node_id="'+selects[0]+'">'+
                             '<input class="form-inline"/>'+
                         '</div>');
                     $("#input-holder").append(new_div_with_input);
-                    var new_ms = attach_magic_suggest(new_div_with_input);
-//                    new_ms.focus();
+                    var new_ms = attach_magic_suggest(new_div_with_input, parent);
                     var new_input = $("input", new_div_with_input)[0];
                     setTimeout(function(){
                         $(new_input).focus();
@@ -40,13 +44,8 @@ $(function() {
                     }, 50);
 
                     console.log(new_input);
-//                    $(new_input).focus();
-//                    $(new_input).select();
             });
-//            $(ms).on("selectionchange", function(a, b){
-//                console.log("another selectionchange");
-//            });
             return ms;
         }
-        attach_magic_suggest($("#level-0"));
+        attach_magic_suggest($("#level-0"), null);
     });
